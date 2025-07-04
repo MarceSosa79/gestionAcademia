e28b8f6ed7e49b957f6fa89bb02546171ef5ee2f
Marcelo <marcelososat79@gmail.com>
2025-07-04 17:45:17 -0300

    Cambios solicitados

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,77 @@
-import java.util.Scanner;
 import models.Course;
 import models.OnlineCourse;
 import models.OnSiteCourse;
+import java.util.Scanner;
 import services.PrinterService;
 
-
 public class Main {
+
     public static void main(String[] args) {
-        //Abro scanner
+        Course[] courses = registerCourses();
+        printCourses(courses);
+    }
+
+    public static Course[] registerCourses() {
         Scanner sc = new Scanner(System.in);
 
-        //Dato cantidad de cursos
-        System.out.println("Ingresar el numero de cursos a registrar:");
+        System.out.println("Ingrese el numero de cursos:");
         int amount = Integer.parseInt(sc.nextLine());
+
         Course[] courseArray = new Course[amount];
 
-        //Controlo cantidad de cursos
-        int index = 0;
-
-        //igual o menor a 0 salgo
-        while (index < amount) {
-            System.out.println("\nCurso #" + (index + 1));
-
-            String courseType = "";
-            boolean valid = false;
-        
-            //Tipo de curso
-            while (!valid) {
-                System.out.println("Ingrese el tipo de curso (Online or OnSite):");
-                courseType = sc.nextLine();
-                if (courseType.equalsIgnoreCase("Online") || courseType.equalsIgnoreCase("OnSite")) {
-                    valid = true;
-                } else {
-                    System.out.println("Tipo invalido, por favor vuelva a ingresar.");
-                }
-            }
-            //Datos del curso
-            System.out.println("Title:");
-            String title = sc.nextLine();
-
-            System.out.println("Duration (hours):");
-            int duration = Integer.parseInt(sc.nextLine());
-
-            System.out.println("Professor (optional):");
-            String professor = sc.nextLine();
-            
-            //Control de campos vacios
-            if (courseType.equalsIgnoreCase("Online")) {
-                System.out.println("Platform:");
-                String platform = sc.nextLine();
-
-                if (professor.isEmpty()) {
-                    courseArray[index] = new OnlineCourse(title, duration, platform);
-                } else {
-                    courseArray[index] = new OnlineCourse(title, duration, professor, platform);
-                }
-
-            } else if (courseType.equalsIgnoreCase("OnSite")) {
-                System.out.println("Room:");
-                String room = sc.nextLine();
-
-                System.out.println("Quota:");
-                int quota = Integer.parseInt(sc.nextLine());
-
-                if (professor.isEmpty()) {
-                    courseArray[index] = new OnSiteCourse(title, duration, room, quota);
-                } else {
-                    courseArray[index] = new OnSiteCourse(title, duration, professor, room, quota);
-                }
-            }
-
-            index++;
+        for (int i = 0; i < amount; i++) {
+            System.out.println("\nCurso #" + (i + 1));
+            courseArray[i] = readCourse(sc);
         }
-        
-        //Imprimo cursos
-        PrinterService printer = new PrinterService();
-        printer.printCourseList(courseArray);
 
         sc.close();
+        return courseArray;
     }
-}        
+
+    public static Course readCourse(Scanner sc) {
+        String type = "";
+        while (true) {
+            System.out.println("Ingrese el tipo de curso (Online / OnSite):");
+            type = sc.nextLine().trim();
+            if (type.equalsIgnoreCase("Online") || type.equalsIgnoreCase("OnSite")) break;
+            System.out.println("Modalidad invalida. Por favor vuelva a ingresar.");
+        }
+
+        System.out.println("Title:");
+        String title = sc.nextLine();
+
+        System.out.println("Duration (hours):");
+        int duration = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Professor (optional):");
+        String professor = sc.nextLine();
+
+        if (type.equalsIgnoreCase("Online")) {
+            System.out.println("Platform:");
+            String platform = sc.nextLine();
+
+            if (professor.isEmpty()) {
+                return new OnlineCourse(title, duration, platform);
+            } else {
+                return new OnlineCourse(title, duration, professor, platform);
+            }
+        } else {
+            System.out.println("Room:");
+            String room = sc.nextLine();
+
+            System.out.println("Quota:");
+            int quota = Integer.parseInt(sc.nextLine());
+
+            if (professor.isEmpty()) {
+                return new OnSiteCourse(title, duration, room, quota);
+            } else {
+                return new OnSiteCourse(title, duration, professor, room, quota);
+            }
+        }
+    }
+
+    public static void printCourses(Course[] courses) {
+        PrinterService printer = new PrinterService();
+        printer.printCourseList(courses);
+    }
+}
